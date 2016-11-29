3dd912c5f6f214f0783127a9c216e1b4b15304a8
xxmfypp <fypp128@163.com>
2016-11-29 19:36:45 +0800

    解析并保存excel内容

--- src/main/java/cc/gavin/grumman/zeta/controller/IndexController.java
+++ src/main/java/cc/gavin/grumman/zeta/controller/IndexController.java
@@ -1,10 +1,18 @@
 package cc.gavin.grumman.zeta.controller;
 
+import cc.gavin.grumman.zeta.util.ExcelUtil;
 import com.jfinal.core.Controller;
 import com.jfinal.kit.PathKit;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.upload.UploadFile;
+import net.sf.json.JSONObject;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by user on 11/4/16.
@@ -31,5 +39,105 @@ public class IndexController extends Controller {
         fileName = fileName.startsWith("/")?webRootPath + fileName:fileDownloadPath + fileName;
         System.out.print(fileName);
         renderFile(fileName);
+    }
+
+    public void upload_tmp(){
+        List<String> errMessage = new ArrayList<String>();
+        try {
+
+            String filePath = "/home/user/Desktop/template.xlsx";
+            Map<String,List<Record>> map =  new HashMap<String, List<Record>>();
+            if (filePath.endsWith("xls")) {
+                ExcelUtil.readXls(filePath);
+            } else if (filePath.endsWith("xlsx")) {
+                map = ExcelUtil.readXlsx(filePath);
+            }
+
+            List<Record> userInfos = map.get("用户信息");
+            List<Record> commodityInfos = map.get("商品信息");
+            List<Record> orderInfos = map.get("订单信息");
+            List<Record> collectionInfos = map.get("收藏信息");
+
+            Db.update("delete from user_info");
+            Db.update("delete from commodity_info");
+            Db.update("delete from order_info");
+            Db.update("delete from collection_info");
+
+
+            for(int i = 0;i<userInfos.size();i++){
+
+                try{
+                    Db.save("user_info","username",userInfos.get(i));
+                }catch (Exception e){
+                    StringBuilder sb =  new StringBuilder("[用户信息]");
+                    sb.append("  用户名:").append(collectionInfos.get(i).getColumns().get("username"));
+                    sb.append("  性别:").append(collectionInfos.get(i).getColumns().get("gender"));
+                    sb.append("  出生日期:").append(collectionInfos.get(i).getColumns().get("birthday"));
+                    sb.append("  发生异常,异常原因：").append(e.getMessage());
+                    errMessage.add(sb.toString());
+                }
+            }
+
+            for(int i = 0;i<commodityInfos.size();i++){
+
+                try{
+                    Db.save("commodity_info","commodity_id",commodityInfos.get(i));
+                }catch (Exception e){
+                    StringBuilder sb =  new StringBuilder("[商品信息]");
+                    sb.append("  商品编号:").append(collectionInfos.get(i).getColumns().get("commodity_id"));
+                    sb.append("  商品名称:").append(collectionInfos.get(i).getColumns().get("name"));
+                    sb.append("  类别:").append(collectionInfos.get(i).getColumns().get("category"));
+                    sb.append("  商品价格:").append(collectionInfos.get(i).getColumns().get("price"));
+                    sb.append("  发生异常,异常原因：").append(e.getMessage());
+                    errMessage.add(sb.toString());
+                }
+            }
+
+            for(int i = 0;i<orderInfos.size();i++){
+
+                try{
+                    Db.save("order_info","order_id",orderInfos.get(i));
+                }catch (Exception e){
+                    StringBuilder sb =  new StringBuilder("[订单信息]");
+                    sb.append("  订单编号:").append(collectionInfos.get(i).getColumns().get("order_id"));
+                    sb.append("  用户名:").append(collectionInfos.get(i).getColumns().get("username"));
+                    sb.append("  所属地区:").append(collectionInfos.get(i).getColumns().get("area"));
+                    sb.append("  订单金额:").append(collectionInfos.get(i).getColumns().get("amount"));
+                    sb.append("  订单日期:").append(collectionInfos.get(i).getColumns().get("create_time"));
+                    sb.append("  发生异常,异常原因：").append(e.getMessage());
+                    errMessage.add(sb.toString());
+                }
+            }
+
+            for(int i = 0;i<collectionInfos.size();i++){
+                try{
+                    Db.save("collection_info","username",collectionInfos.get(i));
+                }catch (Exception e){
+                    StringBuilder sb =  new StringBuilder("[收藏信息]");
+                    sb.append("  用户名:").append(collectionInfos.get(i).getColumns().get("username"));
+                    sb.append("  商品编号:").append(collectionInfos.get(i).getColumns().get("commodity_id"));
+                    sb.append("  收藏日期:").append(collectionInfos.get(i).getColumns().get("create_time"));
+                    sb.append("  发生异常,异常原因：").append(e.getMessage());
+                    errMessage.add(sb.toString());
+                }
+
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+            errMessage.add(e.getMessage());
+        }
+
+        JSONObject resultMsg = new JSONObject();
+
+        if(errMessage.isEmpty()){
+            resultMsg.put("status",0);
+            resultMsg.put("msg","上传成功");
+        }else{
+            resultMsg.put("status",1);
+            resultMsg.put("msg",errMessage);
+        }
+        renderJson(errMessage);
+
     }
 }
--- src/main/java/cc/gavin/grumman/zeta/util/ExcelUtil.java
+++ src/main/java/cc/gavin/grumman/zeta/util/ExcelUtil.java
@@ -4,11 +4,22 @@ package cc.gavin.grumman.zeta.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import com.alibaba.druid.util.StringUtils;
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 
 /**
@@ -17,11 +28,10 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 public class ExcelUtil {
 
     public static void main(String[] args) throws IOException {
-        String filePath = "/home/xxmfypp/Desktop/temp.xls";
-        readXls(filePath);
+
     }
 
-    private static void readXls(String filePath) throws IOException{
+    public static void readXls(String filePath) throws IOException {
         InputStream is = new FileInputStream(filePath);
         HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
 
@@ -44,14 +54,158 @@ public class ExcelUtil {
         }
     }
 
+
+    public static Map<String,List<Record>> readXlsx(String path) throws IOException {
+        Map<String,List<Record>> map = new HashMap<String,List<Record>>();
+        InputStream is = new FileInputStream(path);
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(is);
+
+
+        // Read the Sheet
+        for (int numSheet = 0; numSheet < xssfWorkbook.getNumberOfSheets(); numSheet++) {
+            XSSFSheet xssfSheet = xssfWorkbook.getSheetAt(numSheet);
+            if (xssfSheet == null) {
+                continue;
+            }
+            if(xssfSheet.getSheetName().equals("用户信息")){
+                map.put("用户信息",getUserInfoRecord(xssfSheet));
+            }else if(xssfSheet.getSheetName().equals("商品信息")){
+                map.put("商品信息",getCommodityInfoRecord(xssfSheet));
+            }else if(xssfSheet.getSheetName().equals("订单信息")){
+                map.put("订单信息",getOrderInfoRecord(xssfSheet));
+            }else if(xssfSheet.getSheetName().equals("收藏信息")){
+                map.put("收藏信息",getCollectionInfoRecord(xssfSheet));
+            }
+        }
+        return map;
+    }
+
+
+    private  static List<Record> getUserInfoRecord(XSSFSheet xssfSheet){
+        XSSFRow title_xssfRow = xssfSheet.getRow(0);
+
+        Map<String,Integer> map  =  getIndexMap(new String[]{"用户名","性别","出生日期"},title_xssfRow);
+
+        List<Record> list_Record = new ArrayList<Record>();
+
+        for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+            XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+            if (xssfRow != null) {
+                Record record = new Record();
+                record.set("username",getValue(xssfRow.getCell(map.get("用户名"))));
+                record.set("gender",getValue(xssfRow.getCell(map.get("性别"))));
+                record.set("birthday",getValue(xssfRow.getCell(map.get("出生日期"))));
+                list_Record.add(record);
+            }
+        }
+        return list_Record;
+    }
+
+    private  static List<Record> getCommodityInfoRecord(XSSFSheet xssfSheet){
+        XSSFRow title_xssfRow = xssfSheet.getRow(0);
+
+        Map<String,Integer> map  =  getIndexMap(new String[]{"商品编号","商品名称","类别","商品价格"},title_xssfRow);
+
+        List<Record> list_Record = new ArrayList<Record>();
+
+        for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+            XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+            if (xssfRow != null) {
+                Record record = new Record();
+                record.set("commodity_id",getValue(xssfRow.getCell(map.get("商品编号"))));
+                record.set("name",getValue(xssfRow.getCell(map.get("商品名称"))));
+                record.set("category",getValue(xssfRow.getCell(map.get("类别"))));
+                record.set("price",getValue(xssfRow.getCell(map.get("商品价格"))));
+                list_Record.add(record);
+            }
+        }
+        return list_Record;
+    }
+
+    private  static List<Record> getOrderInfoRecord(XSSFSheet xssfSheet){
+        XSSFRow title_xssfRow = xssfSheet.getRow(0);
+
+        Map<String,Integer> map  =  getIndexMap(new String[]{"订单编号","用户名","所属地区","订单金额","订单日期"},title_xssfRow);
+
+        List<Record> list_Record = new ArrayList<Record>();
+
+        for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+            XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+            if (xssfRow != null) {
+                Record record = new Record();
+                record.set("order_id",getValue(xssfRow.getCell(map.get("订单编号"))));
+                record.set("username",getValue(xssfRow.getCell(map.get("用户名"))));
+                record.set("area",getValue(xssfRow.getCell(map.get("所属地区"))));
+                record.set("amount",getValue(xssfRow.getCell(map.get("订单金额"))));
+                record.set("create_time",getValue(xssfRow.getCell(map.get("订单日期"))));
+                list_Record.add(record);
+            }
+        }
+        return list_Record;
+    }
+
+    private  static List<Record> getCollectionInfoRecord(XSSFSheet xssfSheet){
+        XSSFRow title_xssfRow = xssfSheet.getRow(0);
+
+        Map<String,Integer> map  =  getIndexMap(new String[]{"用户名","商品编号","收藏日期"},title_xssfRow);
+
+        List<Record> list_Record = new ArrayList<Record>();
+
+        for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+            XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+            if (xssfRow != null) {
+                Record record = new Record();
+                record.set("username",getValue(xssfRow.getCell(map.get("用户名"))));
+                record.set("commodity_id",getValue(xssfRow.getCell(map.get("商品编号"))));
+                record.set("create_time",getValue(xssfRow.getCell(map.get("收藏日期"))));
+                list_Record.add(record);
+            }
+        }
+        return list_Record;
+    }
+
+
     private static String getValue(HSSFCell hssfCell) {
         if (hssfCell.getCellType() == hssfCell.CELL_TYPE_BOOLEAN) {
             return String.valueOf(hssfCell.getBooleanCellValue());
         } else if (hssfCell.getCellType() == hssfCell.CELL_TYPE_NUMERIC) {
-            return String.valueOf(hssfCell.getNumericCellValue());
+            return String.valueOf(hssfCell.getStringCellValue());
         } else {
             return String.valueOf(hssfCell.getStringCellValue());
         }
+    }
+
+
+    private static String getValue(XSSFCell xssfRow) {
+        if (xssfRow.getCellType() == xssfRow.CELL_TYPE_BOOLEAN) {
+            return String.valueOf(xssfRow.getBooleanCellValue());
+        } else if (xssfRow.getCellType() == xssfRow.CELL_TYPE_NUMERIC) {
+            return String.valueOf(xssfRow.getRawValue());
+        } else {
+            return String.valueOf(xssfRow.getStringCellValue());
+        }
+    }
+
+    private static Map<String,Integer> getIndexMap(String[] param,XSSFRow xssfRow){
+        Map<String,Integer> map  =  new HashMap<String,Integer>();
+        for(int i=0;i<param.length;i++){
+            for(int j=0;j<100;j++){
+                XSSFCell cell =  xssfRow.getCell(j);
+                if(cell==null){
+                    break;
+                }
+
+                String value = getValue(cell);
+                if(!StringUtils.isEmpty(value)){
+                    if(value.equals(param[i])){
+                        map.put(param[i],j);
+                    }
+                }else{
+                    break;
+                }
+            }
+        }
+        return map;
     }
 
 }
